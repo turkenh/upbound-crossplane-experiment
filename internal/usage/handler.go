0e60616776d46aa5005014b721ef49ba400010ed
Hasan Turken <turkenh@gmail.com>
2023-06-01 17:26:41 +0300

    Webhook for checking usages
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

---
+++ internal/usage/handler.go
@@ -0,0 +1,97 @@
+/*
+Copyright 2023 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package usage contains the handler for the usage webhook.
+package usage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	xpunstructured "github.com/crossplane/crossplane-runtime/pkg/resource/unstructured"
+	"github.com/crossplane/crossplane/apis/apiextensions/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"net/http"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/crossplane/crossplane-runtime/pkg/controller"
+)
+
+// handler implements the admission handler for Composition.
+type handler struct {
+	reader  client.Reader
+	options controller.Options
+}
+
+// SetupWebhookWithManager sets up the webhook with the manager.
+func SetupWebhookWithManager(mgr ctrl.Manager, options controller.Options) error {
+
+	/*	indexer := mgr.GetFieldIndexer()
+		if err := indexer.IndexField(context.Background(), &extv1.CustomResourceDefinition{}, "spec.group", func(obj client.Object) []string {
+			return []string{obj.(*extv1.CustomResourceDefinition).Spec.Group}
+		}); err != nil {
+			return err
+		}
+		if err := indexer.IndexField(context.Background(), &extv1.CustomResourceDefinition{}, "spec.names.kind", func(obj client.Object) []string {
+			return []string{obj.(*extv1.CustomResourceDefinition).Spec.Names.Kind}
+		}); err != nil {
+			return err
+		}*/
+
+	mgr.GetWebhookServer().Register("/validate-no-usages",
+		&webhook.Admission{Handler: &handler{
+			reader:  xpunstructured.NewClient(mgr.GetClient()),
+			options: options,
+		}})
+
+	return nil
+}
+
+// Handle handles the admission request, validating the Composition.
+func (h *handler) Handle(ctx context.Context, request admission.Request) admission.Response {
+	switch request.Operation {
+	case admissionv1.Create, admissionv1.Update, admissionv1.Connect:
+		return admission.Errored(http.StatusBadRequest, errors.New("unexpected operation"))
+	case admissionv1.Delete:
+		u := &unstructured.Unstructured{}
+		if err := u.UnmarshalJSON(request.OldObject.Raw); err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+		return h.validateNoUsages(ctx, u)
+	default:
+		return admission.Errored(http.StatusBadRequest, errors.New("unexpected operation"))
+	}
+}
+
+func (h *handler) validateNoUsages(ctx context.Context, u *unstructured.Unstructured) admission.Response {
+	fmt.Println("Checking for usages")
+
+	usageList := &v1alpha1.UsageList{}
+	if err := h.reader.List(ctx, usageList); err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	for _, usage := range usageList.Items {
+		if usage.Spec.Of.APIVersion == u.GetAPIVersion() && usage.Spec.Of.Kind == u.GetKind() && usage.Spec.Of.Name == u.GetName() {
+			return admission.Denied(fmt.Sprintf("The resource has useges by %s/%s", u.GroupVersionKind().String(), usage.Spec.By.Name))
+		}
+	}
+	return admission.Allowed("")
+}
